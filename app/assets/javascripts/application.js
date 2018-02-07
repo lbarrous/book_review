3f654984053a69cdbe8d6ebc60a115ece3473142
Alvaro Perez Garcia <alvarop.garcia@alu.uhu.es>
2018-02-07 23:17:53 +0000

    Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery.min.js
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require popper.min.js
+//= require bootstrap.min.js
+//= require custom.js
